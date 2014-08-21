c3ed4f6d9bb93a9482d4a5d67386f4258504a176
royhsueztable <royhsu@Roys-Retina-MacBook-Pro-13.local>
2014-08-21 10:48:32 +0800

    Initial Commit

---
+++ RHUICollectionView/RHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RHAppDelegate.h
+//  RHUICollectionView
+//
+//  Created by Roy Hsu on 2014/8/21.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RHUICollectionViewTests/RHUICollectionViewTests.m
@@ -0,0 +1,34 @@
+//
+//  RHUICollectionViewTests.m
+//  RHUICollectionViewTests
+//
+//  Created by Roy Hsu on 2014/8/21.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RHUICollectionViewTests : XCTestCase
+
+@end
+
+@implementation RHUICollectionViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
